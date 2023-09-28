33e6b7e51c4752c06cd29a94cabc1f9ea540dd3f
Sammy Maldonado <sammyalejandro.m.p@gmail.com>
2023-09-27 23:42:47 -0300

    Proyecto final, actualizado 27-09-2023 - 23:42hrs

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ import __dirname from './utils.js';
 const app = express();
 //Defino que front puede entrar a mi servidor mediante CORS
 app.use(cors({
-  origin: config.react.BASEURL,
+  origin: '*',
   credentials: true
 }));
 const PORT = config.app.PORT || 8080;
